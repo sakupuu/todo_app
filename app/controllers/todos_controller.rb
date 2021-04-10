fe642db41adc82db54f37d0b41ca57ca0cc934f5
sakupuu <saku.yuzu.113@gmail.com>
2021-04-10 23:05:12 +0900

    privateメソッドを追加しました

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -15,4 +15,9 @@ class TodosController < ApplicationController
   def create
     @todo = Todo.create todo_params
   end
+
+  private
+    def todo_params
+      params.require(:todo).permit(:title, :description)
+    end
 end
